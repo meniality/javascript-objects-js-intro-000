1e884bcf83b339757cbbc745ac76a1ddb62c7214
patrick hoehn <patrh_99@hotmail.com>
2019-09-29 17:59:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { billyJoel : "piano man"};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return object.assign({}, playlist, artistName: [songTitle})
+  return object.assign({}, playlist, artistName: [songTitle]})
 }
